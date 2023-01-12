b29b000966e32cd6ec313d030754816084f1f783
ksr0818 <ksr940818@gmail.com>
2023-01-12 10:22:26 +0900

    0112_coding

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String [] arr = new String[] {"Queen","King"};
+
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put(arr[0],arr[arr.length-1]);
+
+        System.out.println(hashMap);
+
+
     }
 }
